2687df116ef666bc862f779d25abe8304c4b65db
Bastien Murot <bmurot@talentsoft.com>
2019-12-09 14:34:43 +0100

    WIP Day Two

---
+++ DayOne/DayOne.Test/ComputeIntcode.cs
@@ -0,0 +1,40 @@
+﻿namespace DayOne.Test
+{
+    using Xunit;
+
+    public class ComputeIntCodes
+    {
+        [Fact]
+        public void ItExists()
+        {
+            var intcode = new Intcode("1,0,0,3,99");
+            int result = intcode.Compute();
+        }
+
+        [Fact]
+        public void GivenAnEmptyOpcodeSetShouldReturnValuesOfZero()
+        {
+            // Arrange
+            var intcode = new Intcode("99");
+
+            // Act
+            int result = intcode.Compute();
+
+            // Assert
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void GivenAnAddSetOfOpcodeShouldReturnValuesOfTwo()
+        {
+            // Arrange
+            var intcode = new Intcode("1, 0, 0, 99");
+
+            // Act
+            int result = intcode.Compute();
+
+            // Assert
+            Assert.Equal(2, result);
+        }
+    }
+}
---
+++ DayOne/DayOne/Intcode.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DayOne
+{
+    public class Intcode
+    {
+        private readonly string _intcode;
+        private List<int[]> _opcodeInstructions;
+        private int _lastValue = 0;
+
+        private const int ADD_OPCODE = 1;
+        private const int PROGRAM_END = 99;
+
+        public Intcode(string intcode)
+        {
+            _intcode = intcode;
+
+            BuildOpcodeInstructions();
+        }
+
+        private void BuildOpcodeInstructions()
+        {
+            var opcodes = _intcode.Split(',');
+
+            _opcodeInstructions = new List<int[]>();
+
+            var instructionsSet = new List<int>();
+
+            foreach(var opcode in opcodes)
+            {
+                var numericOpcode = int.Parse(opcode);
+                if (numericOpcode == PROGRAM_END)
+                {
+                    instructionsSet.Add(PROGRAM_END);
+                    _opcodeInstructions.Add(instructionsSet.ToArray());
+                }
+            }
+        }
+
+        public int Compute()
+        {
+            int currentPointer = 0;
+            int currentSetIndex = 0;
+
+            int[] currentSet = _opcodeInstructions[currentSetIndex];
+
+            while(currentSet.Length != 1 && currentSet.First() != PROGRAM_END)
+            {
+                if (currentSet[0] == ADD_OPCODE)
+                {
+
+                }
+            }
+
+            return _lastValue;
+        }
+    }
+}
